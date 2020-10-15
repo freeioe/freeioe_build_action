cbdb84f54ea5a38f756b4f007ee0e9440e2f1d08
Dirk Chang <dirk@kooiot.com>
2020-10-15 17:28:42 +0800

    fixed __install folder path

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ echo "Action: $1"
 
 # Output the filename
 # filename=`ls *.tar.gz | grep -v -- -dbgsym`
-filename=`ls __install/* | grep -v -- -dbgsym`
+filename=`ls /__install/* | grep -v -- -dbgsym`
 echo ::set-output name=filename::$filename
